68d47208dea089fc83749a422c2fb42b71976199
fl <flier@techfak.uni-bielefeld.de>
2015-12-16 18:01:09 +0100

    Integration

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,17 @@
  *
  */
 
-
 #include <opencv2/opencv.hpp>
 
 #include <iostream>
 #include <sstream>
+#include <thread>
+
+#include <boost/program_options/options_description.hpp>
+#include <boost/program_options/parsers.hpp>
+#include <boost/program_options/variables_map.hpp>
+#include <boost/tokenizer.hpp>
+#include <boost/token_functions.hpp>
 
 #include <nmpt/BlockTimer.h>
 #include <nmpt/FastSalience.h>
@@ -33,15 +39,138 @@
 
 using namespace std;
 using namespace cv;
+using namespace boost;
+using namespace boost::program_options;
+
+class Faces
+{
+    public:
+      Faces();
+      ~Faces();
+      void getFaces(cv::Mat img);
+      void setPath(String path);
+
+    protected:
+      // DLIB
+      dlib::frontal_face_detector detector;
+      dlib::shape_predictor pose_model;
+      dlib::image_window win;
+};
+
+Faces::Faces(){}
+Faces::~Faces(){}
+
+void Faces::setPath(String path)
+{
+    detector = dlib::get_frontal_face_detector();
+    try {
+        dlib::deserialize(path) >> pose_model;
+    } catch(dlib::serialization_error& e) {
+        cout << "You need dlib's default face landmarking model file to run this example." << endl;
+        cout << "You can get it from the following URL: " << endl;
+        cout << "http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2" << endl;
+        cout << endl << e.what() << endl;
+    }
+}
+
+
+void Faces::getFaces(cv::Mat _img)
+{
+    dlib::cv_image<dlib::bgr_pixel> cimg(_img);
+    // Detect faces
+    std::vector<dlib::rectangle> faces = detector(cimg);
+    // Find the pose of each face.
+    std::vector<dlib::full_object_detection> shapes;
+    for (unsigned long i = 0; i < faces.size(); ++i) {
+        shapes.push_back(pose_model(cimg, faces[i]));
+    }
+    // Display it all on the screen
+    win.clear_overlay();
+    win.set_image(cimg);
+    win.add_overlay(render_face_detections(shapes));
+}
+
 
 int main (int argc, char * const argv[])
 {
+    String dlib_path = "None";
+    bool faces_flag = false;
+    bool saliency_flag = false;
+
+    // Programm options
+    try {
+
+        options_description general("general options");
+        general.add_options()
+            ("help", "Show this help")
+            ("version", "0.1")
+            ;
+
+        options_description dlib("dlib options");
+        dlib.add_options()
+            ("dlib", value<string>(), "the dlib pose model path")
+            ;
+
+        options_description saliency("saliency detection options");
+        saliency.add_options()
+            ("saliency", value<string>(), "detect most salient point ON|OFF")
+            ;
+
+        options_description faces("face detection options");
+        faces.add_options()
+            ("faces", value<string>(), "detect faces ON|OFF")
+            ;
+
+        options_description all("Allowed options");
+        all.add(general).add(dlib).add(saliency).add(faces);
+
+        options_description visible("Allowed options");
+        visible.add(general).add(dlib).add(saliency).add(faces);
+
+        variables_map vm;
+
+        store(parse_command_line(argc, argv, all), vm);
+
+        if (vm.count("help"))
+        {
+            cout << visible;
+            return 0;
+        }
+
+        if (vm.count("faces")) {
+            cout << ">>> face detection is ON" << "\n";
+            faces_flag = true;
+            if (vm.count("dlib")) {
+                const string& s = vm["dlib"].as<string>();
+                dlib_path = s;
+                cout << "    dlib pose model path is: " << s << "\n";
+            } else {
+                cout << ">>> ERROR: dlib pose model path NOT set \n";
+                faces_flag = false;
+                return 0;
+            }
+        } else {
+            cout << ">>> face detection is OFF" << "\n";
+            faces_flag = false;
+        }
+
+        if (vm.count("saliency")) {
+            cout << ">>> saliency detection is ON" << "\n";
+            saliency_flag = true;
+        } else {
+            cout << ">>> saliency detection is OFF" << "\n";
+            saliency_flag = false;
+        }
+
+     } catch(std::exception& e) { cout << e.what() << "\n"; }
+
     Size imSize(320,240);
     BlockTimer bt;
 
-    /* Open capture */
+    //CV set capture device
     VideoCapture capture;
-    int usingCamera = NMPTUtils::getVideoCaptureFromCommandLineArgs(capture, argc, (const char**) argv);
+\
+    int usingCamera = NMPTUtils::getVideoCaptureFromCommandLineArgs(capture, argc, (const char**)argv);
     if (!usingCamera--) return 0;
 
     FastSalience salTracker;
@@ -50,34 +179,33 @@ int main (int argc, char * const argv[])
     vector<double> lqrpt(2,.5);
     salientSpot.setTrackerTarget(lqrpt);
 
-    /* Set capture to desired width/height */
+    //CV set capture to desired width/height
     if (usingCamera) {
         capture.set(CV_CAP_PROP_FRAME_WIDTH, imSize.width);
         capture.set(CV_CAP_PROP_FRAME_HEIGHT, imSize.height);
     }
+
     bt.blockRestart(1);
-    cv::Mat im, im2, viz, sal, temp;
+    cv::Mat im, im2, viz, sal;
 
     // ROS
     ros::init(argc, (char **) argv, "gazetools");
     ros::NodeHandle n;
-
     ros::Publisher pub_s = n.advertise<sensor_msgs::RegionOfInterest>("gazetools/saliency", 10);
     ros::Publisher pub_f = n.advertise<people_msgs::People>("gazetools/faces", 10);
 
     // DLIB
-    // Load face detection and pose estimation models.
-    dlib::frontal_face_detector detector = dlib::get_frontal_face_detector();
-    dlib::shape_predictor pose_model;
-    dlib::deserialize("shape_predictor_68_face_landmarks.dat") >> pose_model;
-    dlib::image_window win;
+    Faces f;
+    if(faces_flag) {
+        f.setPath(dlib_path);
+    }
+
+    // thread c_thread;
 
     while (waitKey(5) <= 0) {
-        try  {
+
             double saltime, tottime;
             capture >> im2;
-            capture >> temp;
-            // im2 >> temp;
             if (usingCamera) {
                 im = im2;
             } else {
@@ -85,97 +213,77 @@ int main (int argc, char * const argv[])
                 resize(im2, im, Size(0,0), ratio, ratio, INTER_NEAREST);
             }
 
-            dlib::cv_image<dlib::bgr_pixel> cimg(temp);
-            // Detect faces
-            std::vector<dlib::rectangle> faces = detector(cimg);
-            // Find the pose of each face.
-            std::vector<dlib::full_object_detection> shapes;
-            for (unsigned long i = 0; i < faces.size(); ++i) {
-                shapes.push_back(pose_model(cimg, faces[i]));
+            /*if(faces_flag && c_thread.get_id() == thread::id()) {
+                //f.getFaces(im);
+                //thread t(bind(Faces::getFaces, f, im));
+                c_thread = thread(bind(&Faces::getFaces, &f, im));
+            }*/
+
+            f.getFaces(im);
+
+            if (saliency_flag) {
+                viz.create(im.rows, im.cols*2, CV_32FC3);
+
+                bt.blockRestart(0);
+                vector<KeyPoint> pts;
+                salTracker.detect(im, pts);
+                saltime = bt.getCurrTime(0) ;
+
+                salTracker.getSalImage(sal);
+
+                double min, max;
+                Point minloc, maxloc;
+                minMaxLoc(sal, &min, &max, &minloc, &maxloc);
+
+                lqrpt[0] = maxloc.x*1.0 / sal.cols;
+                lqrpt[1] = maxloc.y*1.0 / sal.rows;
+
+                salientSpot.setTrackerTarget(lqrpt);
+
+                Mat vizRect = viz(Rect(im.cols,0,im.cols, im.rows));
+                cvtColor(sal, vizRect, CV_GRAY2BGR);
+
+                vizRect = viz(Rect(0, 0, im.cols, im.rows));
+                im.convertTo(vizRect,CV_32F, 1./256.);
+
+                for (size_t i = 0; i < pts.size(); i++) {
+                    circle(vizRect, pts[i].pt, 2, CV_RGB(0,255,0));
+                }
+
+                salientSpot.updateTrackerPosition();
+                lqrpt = salientSpot.getCurrentPosition();
+
+                circle(vizRect, Point(lqrpt[0]*sal.cols, lqrpt[1]*sal.rows), 6, CV_RGB(0,0,255));
+                circle(vizRect, Point(lqrpt[0]*sal.cols, lqrpt[1]*sal.rows), 5, CV_RGB(0,0,255));
+                circle(vizRect, Point(lqrpt[0]*sal.cols, lqrpt[1]*sal.rows), 4, CV_RGB(255,255,0));
+                circle(vizRect, Point(lqrpt[0]*sal.cols, lqrpt[1]*sal.rows), 3, CV_RGB(255,255,0));
+
+                vizRect = viz(Rect(im.cols,0,im.cols, im.rows));
+                cvtColor(sal, vizRect, CV_GRAY2BGR);
+                if (usingCamera) flip(viz, viz, 1);
+
+                tottime = bt.getCurrTime(1);
+                bt.blockRestart(1);
+
+                stringstream text;
+                text << "FastSUN: " << (int)(saltime*1000) << " ms ; Total: " << (int)(tottime*1000) << " ms.";
+
+                putText(viz, text.str(), Point(20,20), FONT_HERSHEY_SIMPLEX, .33, Scalar(255,0,255));\
+
+                cout << "Most Salient Point: X " << lqrpt[0]*sal.cols << " Y " << lqrpt[1]*sal.cols << endl;
+
+                sensor_msgs::RegionOfInterest roi_msg;
+                roi_msg.x_offset = lqrpt[0]*sal.cols;
+                roi_msg.y_offset = lqrpt[1]*sal.cols;
+                roi_msg.height = 1;
+                roi_msg.width = 1;
+
+                pub_s.publish(roi_msg);
+
+                imshow("SRG-Tools || NMPT Salience || Press Q to Quit", viz);
             }
-            // Display it all on the screen
-            win.clear_overlay();
-            win.set_image(cimg);
-            win.add_overlay(render_face_detections(shapes));
 
-            viz.create(im.rows, im.cols*2, CV_32FC3);
-
-            bt.blockRestart(0);
-            vector<KeyPoint> pts;
-            salTracker.detect(im, pts);
-            saltime = bt.getCurrTime(0) ;
-
-            salTracker.getSalImage(sal);
-
-
-            double min, max;
-            Point minloc, maxloc;
-            minMaxLoc(sal, &min, &max, &minloc, &maxloc);
-
-            lqrpt[0] = maxloc.x*1.0 / sal.cols;
-            lqrpt[1] = maxloc.y*1.0 / sal.rows;
-
-            salientSpot.setTrackerTarget(lqrpt);
-
-            Mat vizRect = viz(Rect(im.cols,0,im.cols, im.rows));
-            cvtColor(sal, vizRect, CV_GRAY2BGR);
-
-            vizRect = viz(Rect(0, 0, im.cols, im.rows));
-            im.convertTo(vizRect,CV_32F, 1./256.);
-
-            for (size_t i = 0; i < pts.size(); i++) {
-                circle(vizRect, pts[i].pt, 2, CV_RGB(0,255,0));
-            }
-
-            salientSpot.updateTrackerPosition();
-            lqrpt = salientSpot.getCurrentPosition();
-
-            circle(vizRect, Point(lqrpt[0]*sal.cols, lqrpt[1]*sal.rows), 6, CV_RGB(0,0,255));
-            circle(vizRect, Point(lqrpt[0]*sal.cols, lqrpt[1]*sal.rows), 5, CV_RGB(0,0,255));
-            circle(vizRect, Point(lqrpt[0]*sal.cols, lqrpt[1]*sal.rows), 4, CV_RGB(255,255,0));
-            circle(vizRect, Point(lqrpt[0]*sal.cols, lqrpt[1]*sal.rows), 3, CV_RGB(255,255,0));
-
-            vizRect = viz(Rect(im.cols,0,im.cols, im.rows));
-            cvtColor(sal, vizRect, CV_GRAY2BGR);
-            if (usingCamera) flip(viz, viz, 1);
-
-            tottime = bt.getCurrTime(1);
-            bt.blockRestart(1);
-
-            stringstream text;
-            text << "FastSUN: " << (int)(saltime*1000) << " ms ; Total: " << (int)(tottime*1000) << " ms.";
-
-            putText(viz, text.str(), Point(20,20), FONT_HERSHEY_SIMPLEX, .33, Scalar(255,0,255));\
-
-            cout << "Most Salient Point: X " << lqrpt[0]*sal.cols << " Y " << lqrpt[1]*sal.cols << endl;
-
-            sensor_msgs::RegionOfInterest roi_msg;
-            roi_msg.x_offset = lqrpt[0]*sal.cols;
-            roi_msg.y_offset = lqrpt[1]*sal.cols;
-            roi_msg.height = 1;
-            roi_msg.width = 1;
-
-            people_msgs::People people_msg;
-            //people_msg.people.
-            //roi_msg.x_offset = lqrpt[0]*sal.cols;
-            //roi_msg.y_offset = lqrpt[1]*sal.cols;
-            //roi_msg.height = 1;
-            //roi_msg.width = 1;
-
-            pub_s.publish(roi_msg);
-            //pub_f.publish(roi_msg);
-            ros::spinOnce();
-
-            imshow("SRG-Tools || Salience", viz);
-
-        }catch(dlib::serialization_error& e) {
-            cout << "You need dlib's default face landmarking model file to run this example." << endl;
-            cout << "You can get it from the following URL: " << endl;
-            cout << "http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2" << endl;
-            cout << endl << e.what() << endl;
-        }
-        catch(exception& e) {
-            cout << e.what() << endl;
-        }
+        // ROS Spinner (send messages)
+        ros::spinOnce();
     }
 }
