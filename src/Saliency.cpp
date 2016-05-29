d31ef83e52abe7589f618be509459c011f3be226
Florian Lier <flier@techfak.uni-bielefeld.de>
2016-05-29 20:41:39 +0200

    Resize for saliency

--- src/Saliency.cpp
+++ src/Saliency.cpp
@@ -106,7 +106,7 @@ void Saliency::getSaliency(bool saliency_flag, bool timing, int throttle) {
             }
 
             // Resize for Saliency to 1/2
-            cv::Size size(im_source.rows/2,im_source.cols/2);
+            cv::Size size(im_source.cols/2,im_source.rows/2);
             cv::resize(im_source, im, size);
 
         }
@@ -121,7 +121,7 @@ void Saliency::getSaliency(bool saliency_flag, bool timing, int throttle) {
             }
 
             // Resize for Saliency to 1/2
-            cv::Size size(im_source.rows/2,im_source.cols/2);
+            cv::Size size(im_source.cols/2,im_source.rows/2);
             cv::resize(im_source, im, size);
         }
 
@@ -135,7 +135,7 @@ void Saliency::getSaliency(bool saliency_flag, bool timing, int throttle) {
             }
 
             // Resize for Saliency to 1/2
-            cv::Size size(im_source.rows/2,im_source.cols/2);
+            cv::Size size(im_source.cols/2,im_source.rows/2);
             cv::resize(im_source, im, size);
         }
 
